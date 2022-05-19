b996da9e5174367ff018ff0a0cc06c812bb9fef1
Riley B <33437467+brust12@users.noreply.github.com>
2022-05-19 00:13:05 -0500

    initial commit

---
+++ ext/src/inject/inject.js
@@ -0,0 +1,40 @@
+/**
+ * @author Riley Brust <brust.developer@gmail.com>
+ * @version 0.0.1
+ * @description Chrome extention to add SMS to RepairQ.
+ * @file inject.js
+ */
+
+
+
+
+function main(){
+    //Check to see if were on a parts page or another page.
+    let url = document.URL;
+ 
+}
+
+main();
+
+/*
+var ttt = document.getElementById("input_0")
+ttt.value +="Hi Tim,
+
+We have your Screen in Stock! Stop in Today, Walk-Ins Welcome!
+
+The cost for your Screen Replacement is: 
+Standard Screen: $100 
+Apple OEM Screen: $200 
+
+OEM Screen will need to be ordered
+THIS IS AN AUTOMATED MESSAGE";
+var ev = new Event("change");
+ttt.dispatchEvent(ev);
+document.getElementById("ib3").click();
+*/
+
+
+
+/*
+document.evaluate("/html/body/div[7]/div[3]/div/div[2]/div[1]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/table/tr/td[1]/div[2]/div[2]/div/div[3]/div/div/div/div/div/div[1]/div[2]/div[3]/div[3]/div/table/tbody/tr/td/table/tbody/tr/td/table[2]/tbody/tr/td/table[2]/tbody/tr/th[2]/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table[3]/tbody/tr/td/table/tbody/tr/th[1]/table/tbody/tr/td/a/span/strong", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+*/
